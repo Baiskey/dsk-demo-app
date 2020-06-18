d843d1a441f5870f8b7f79b227d7b1a137ae688d
Rkull <baiskey.developer@gmail.com>
2020-06-18 22:34:26 +0200

    Simple endpoint implementation

--- src/main/java/com/calvaquality/SimpleController.java
+++ src/main/java/com/calvaquality/SimpleController.java
@@ -1,13 +1,15 @@
 package com.calvaquality;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
 
+    @CrossOrigin
     @GetMapping(value = "/")
     public String getHelloWorld() {
-        return "Hello World";
+        return "Hello World, my friend";
     }
 }
